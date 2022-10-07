21022ffba6675489af692caa53aa99cc29b268a3
Markus Ort <39794659+MarkusTheOrt@users.noreply.github.com>
2022-10-07 06:28:28 +0200

    Update Constants.ts

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -4,7 +4,7 @@ export default {
     /^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])T(0[1-9]|1[0-9]|2[0-3]):(0[0-9]|[0-5][0-9]):(0[0-9]|[0-5][0-9])Z$/,
   maxTimeApart: 100 * 1000, // 100 Seconds
   defTimeApart: 100_000_000_000, // Default time Apart
-  futureProjection: 3 * 60 * 1000, // 5 Minutes
+  futureProjection: 5 * 60 * 1000, // 5 Minutes
   attachments: [
     { name: "Bongocat.mp4", url: "https://fia.ort.dev/F1themecats.mp4" },
   ],
